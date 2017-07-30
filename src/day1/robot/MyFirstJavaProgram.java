94f1d09217535b560f1cb3c74e425ececfe1c8bb
Love4653 <league@WTS-IM-6.attlocal.net>
2017-07-30 15:28:32 -0700

    Day 1

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
-	}
+Robot r2d2=new Robot();
+	r2d2.penDown();
+    r2d2.setSpeed(1);
+	r2d2.sparkle();
+  for (int i = 0; i <8; i++) {
+	r2d2.turn(45);
+	r2d2.move(100);
+	r2d2.sparkle();
+}
+}
 }
